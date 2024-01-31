aeb79e09549263defd7874088855e68d0587aff0
KatePril <ket14pril14@gmail.com>
2024-01-31 11:10:58 +0200

    Add article

--- app/src/main/java/com/example/composearticle/MainActivity.kt
+++ app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -3,13 +3,22 @@ package com.example.composearticle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composearticle.ui.theme.ComposeArticleTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +31,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ComposeImage(
+                        title = stringResource(R.string.article_title),
+                        paragraphOne = stringResource(R.string.paragraph_one),
+                        paragraphTwo = stringResource(R.string.paragraph_two)
+                    )
                 }
             }
         }
@@ -30,17 +43,68 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun ComposableText(
+    title: String,
+    paragraphOne: String,
+    paragraphTwo: String,
+    modifier: Modifier = Modifier,
+) {
+    Column(
         modifier = modifier
-    )
+    ) {
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = paragraphOne,
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = paragraphTwo,
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
+    }
+
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ComposeImage(
+    title: String,
+    paragraphOne: String,
+    paragraphTwo: String,
+    modifier: Modifier = Modifier
+) {
+    val image = painterResource(R.drawable.bg_compose_background)
+    Column(
+        modifier = modifier
+    ) {
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+        ComposableText(
+            title = title,
+            paragraphOne = paragraphOne,
+            paragraphTwo = paragraphTwo
+        )
+    }
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
+@Composable
+fun ComposeArticlePreview() {
     ComposeArticleTheme {
-        Greeting("Android")
+        ComposeImage(
+            title = stringResource(R.string.article_title),
+            paragraphOne = stringResource(R.string.paragraph_one),
+            paragraphTwo = stringResource(R.string.paragraph_two)
+        )
     }
 }
